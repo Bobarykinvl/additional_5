800775e8b310f9c3a28ee58559cc3fcd0c8de5b5
Bobarykinvl <bobarykinvl@gmail.com>
2018-02-25 21:38:55 +0300

    13passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ 	var array = str.split('');
+	if(array.length % 2 != 0){
+		return false;
+	}
+	var check_count = 0;
+	var check_count_para = 0;
+	
+	for(var i = 0; i < array.length; i++){
+		if(array[i] == '('){
+			check_count++;
+			console.log( 'check_count:  ', check_count)
+		}
+	}
+	console.log(array);
+		
+	for(var j = 0; j < array.length; j++){
+		if(array[j] == ')'){
+			check_count_para++;
+			console.log('check_count_para:  ' + check_count_para);
+		}
+	}
+		
+		if(check_count == check_count_para){
+			return true;
+		} else return false;
+	
+	
+	
 }
